523f36d3a17a323418799414e73447af6682035c
Arnoldo Gonzalez <arnoldogonzalezroman@gmail.com>
2022-07-26 21:11:02 -0400

    first steps to complete the accessibility for index page

--- src/public/js/register/register.js
+++ src/public/js/register/register.js
@@ -1,16 +1,19 @@
 const $form = document.getElementById("form")
 const $submitBtn = document.getElementById("submit-btn")
-const invalidForName = /\D/
+const invalidForName = /\d/
 const invalidForEmail = /\s/
-const invalidForAll = /\W|\/|\\/
+const invalidForAll = /\/|\\/
 
 $form.addEventListener("submit", (e) => {
     const formData = new FormData($form)
     const objData = Object.fromEntries(formData)
     const { name, email } = objData
 
+    console.log({name, email})
+
     if (!name || !email || invalidForAll.test(name) || invalidForAll.test(email) || invalidForName.test(name) || invalidForEmail.test(email)) {
         e.preventDefault()
+        return
     }
 
     $submitBtn.classList.add("active")
